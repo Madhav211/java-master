eecda1320ec120bbd22dd98ef9ae9c15c4b25779
Madhav211 <uec20136@rmd.ac.in>
2022-11-01 20:23:29 +0530

    done basic program

---
+++ class1.java
@@ -0,0 +1,11 @@
+/**
+ * class1
+ */
+public class class1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("begineer");
+
+    }
+}
